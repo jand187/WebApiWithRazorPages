ab1b95240fcda4b286e2501fbd19a8048da2ed32
Jan Daniel Andersen <jand187@gmail.com>
2014-04-14 11:11:53 +0200

    basis Web Api setup.

---
+++ Website/Api/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using RazorEngine;
+using Encoding = System.Text.Encoding;
+
+namespace Website.Api
+{
+	public class HomeController : ApiController
+	{
+		public HttpContent Index()
+		{
+			var template = "Hello @Model.Name! Welcome to Web API and Razor!";
+			var result = Razor.Parse(template, new {Name = "World"});
+
+			return new StringContent(result, Encoding.UTF8, "text/html");
+		}
+	}
+}
